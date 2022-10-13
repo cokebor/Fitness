920d14180e06d8a2d090b6ba7a4d55ccfb752cbe
Jorge Bordarampé <jbordarampe@gmail.com>
2022-10-12 21:34:21 -0300

    Delete FitnessClub Back directory

--- FitnessClub Back/src/main/java/com/fitness/repositorios/ComprobanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitness.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fitness.modelo.Comprobante;
-
-public interface ComprobanteRepository extends JpaRepository<Comprobante, Integer> {
-
-}
--- FitnessClub Back/src/main/java/com/fitness/repositorios/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitness.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fitness.modelo.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer>{
-
-}
